b93d395b851534f0dc99eb97354286d7b30eb597
Taylor Thompson <jamestthompson3@gmail.com>
2020-01-07 17:13:38 +0200

    updates and simplifies post styles, adds libuv post

--- blogheader.js
+++ blogheader.js
@@ -4,9 +4,10 @@
   header.style.height = "40px";
   header.style.zIndex = "9999";
   header.innerHTML =
-    "<a href='/'><h4 style='padding: 0px 8px;'>← Back to Home</h4></a>";
+    "<a href='/'><h4 style='padding: 0px 8px; font-size: 1rem;'>← Back to Home</h4></a>";
   header.ariaLabel = "back";
   header.role = "button";
+  header.style.fontFamily = "monospace";
   const body = document.querySelector("body");
   const main = document.querySelector(".mainwrapper");
   body.insertBefore(header, body.childNodes[0]);
